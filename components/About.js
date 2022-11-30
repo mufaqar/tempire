bee6b0e217b76b9d57035c50b2c70be6ab2024f3
Mufaqar Islam <mufaqar@gmail.com>
2022-11-30 19:48:25 +0500

    added

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import AboutImg from '../public/images/main.png';
+import AboutImg from '../public/images/bridge.jpg';
 
 export default function About() {
   return (
@@ -9,24 +9,18 @@ export default function About() {
         id="about-us"
         className=" md:pt-[76px] md:pb-[63px] py-[43px] px-4"
       >
-        <div className="container mx-auto grid md:grid-cols-2 grid-cols-1 gap-10">
-          <div className="md:pr-10 ">
+        <div className="container mx-auto grid md:grid-cols-2 grid-cols-1 gap-10 ]">
+          <div className="md:pr-10 mt-[25px">
             <h2 className="md:text-[37.5px] md:leading-[1.25] font-bold text-2xl trackind-wide text-[#333333] mb-[29px]">
-              About Tempire
+              About TEMPIRE®
             </h2>
             <div className="md:w-[80%] w-full">
               <p className="text-sm font-medium leading-[2] tracking-wide text-[#333333] mb-10">
-                Tempire is a Professional Mover Platform. Here we will provide
-                you only interesting content, which you will like very much.
-                We're dedicated to providing you the best of Mover, with a focus
-                on dependability and shipping. We're working to turn our passion
-                for Mover into a booming online website. We hope you enjoy our
-                Mover as much as we enjoy offering them to you.
-              </p>
-              <p className="text-sm font-medium leading-[2] tracking-wide text-[#333333] mb-10">
-                Class aptent taciti sociosqu ad litora torquent per conubia
-                nostra, per inceptos himenaeos. Aenean pellentesque tempor
-                libero eget rhoncus.
+                Our flexibility at TEMPIRE® allows our clients to make the best
+                decisions. We work diligently and closely with each client to
+                source out competitive equipment prices along with partnering
+                with multiple lenders to explore all options so our clients can
+                confidently secure assets for their growing businesses.
               </p>
             </div>
           </div>
