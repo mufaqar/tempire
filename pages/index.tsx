4cecb3629e27ddbe0aba504d40a784362eba08bb
Mufaqar Islam <mufaqar@gmail.com>
2022-11-30 18:30:47 +0500

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/header';
 import Stats from '../components/Stats';
 import About from '../components/About';
-import Pricing from '../components/Pricing';
+
 import FAQs from '../components/Faqs';
 import Footer from '../components/footer';
 import Slider from '../components/Hero_Slider';
@@ -14,8 +14,6 @@ export default function Home() {
         <title>Tempire</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
-
       <Header />
       <Slider />
       <Stats />
