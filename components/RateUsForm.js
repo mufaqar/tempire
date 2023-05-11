b88103e0b720e99b09c07ea7a7c0e30a573bdeae
mufaqar <mufaqar@gmail.com>
2023-05-11 15:18:06 +0500

    Rating

--- components/RateUsForm.js
+++ components/RateUsForm.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { client } from "../config/client";
-import { useState } from "react";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { client } from '../config/client';
+import { useState } from 'react';
 
 export default function RateUsForm() {
-
   const [loading, setLoading] = useState(false);
   const [imageAsset, setImageAsset] = useState();
   const [wrongImageType, setWrongImageType] = useState(false);
-  const [status, setStatus] = useState()
+  const [status, setStatus] = useState();
 
-  console.log("🚀 ~ file: RateUsForm.js:9 ~ RateUsForm ~ imageAsset:", imageAsset)
+  console.log(
+    '🚀 ~ file: RateUsForm.js:9 ~ RateUsForm ~ imageAsset:',
+    imageAsset
+  );
 
   const {
     register,
@@ -22,11 +24,20 @@ export default function RateUsForm() {
   const uploadImage = (e) => {
     const selectedFile = e.target.files[0];
     // uploading asset to sanity
-    if (selectedFile.type === 'image/png' || selectedFile.type === 'image/svg' || selectedFile.type === 'image/jpeg' || selectedFile.type === 'image/gif' || selectedFile.type === 'image/tiff') {
+    if (
+      selectedFile.type === 'image/png' ||
+      selectedFile.type === 'image/svg' ||
+      selectedFile.type === 'image/jpeg' ||
+      selectedFile.type === 'image/gif' ||
+      selectedFile.type === 'image/tiff'
+    ) {
       setWrongImageType(false);
       setLoading(true);
       client.assets
-        .upload('image', selectedFile, { contentType: selectedFile.type, filename: selectedFile.name })
+        .upload('image', selectedFile, {
+          contentType: selectedFile.type,
+          filename: selectedFile.name,
+        })
         .then((document) => {
           setImageAsset(document);
           setLoading(false);
@@ -43,7 +54,7 @@ export default function RateUsForm() {
   const onSubmit = (data) => {
     const { email, message, name, position, rateing } = data;
     const doc = {
-      _type: "rateing",
+      _type: 'rateing',
       icon: {
         _type: 'image',
         asset: {
@@ -60,13 +71,13 @@ export default function RateUsForm() {
 
     client.create(doc).then((res) => {
       console.log(`Review is Created ${res._id}`);
-      resetField("name")
-      resetField("email")
-      resetField("position")
-      resetField("rateing")
-      resetField("message")
-      setImageAsset()
-      setStatus(200)
+      resetField('name');
+      resetField('email');
+      resetField('position');
+      resetField('rateing');
+      resetField('message');
+      setImageAsset();
+      setStatus(200);
     });
   };
 
@@ -76,21 +87,6 @@ export default function RateUsForm() {
         <div className="md:w-1/2 mx-auto bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="relative mb-4">
-              <label
-                htmlFor="name"
-                className="leading-7 block text-sm text-gray-600"
-              >
-                Icon
-              </label>
-              <input
-                type="file"
-                name="upload-image"
-                onChange={uploadImage}
-                className=""
-              />
-            </div>
-
-            <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-600">
                 Name
               </label>
@@ -98,7 +94,7 @@ export default function RateUsForm() {
                 type="text"
                 id="name"
                 name="name"
-                {...register("name")}
+                {...register('name')}
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#FDCD60] focus:ring-2 focus:ring-[#FDCD60] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 placeholder="enter your name "
               />
@@ -115,9 +111,23 @@ export default function RateUsForm() {
                 type="email"
                 id="email"
                 name="email"
-                {...register("email")}
+                {...register('email')}
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#FDCD60] focus:ring-2 focus:ring-[#FDCD60] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 placeholder="enter your email address "
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="name"
+                className="leading-7 block text-sm text-gray-600"
+              >
+                Company Logo
+              </label>
+              <input
+                type="file"
+                name="upload-image"
+                onChange={uploadImage}
+                className=""
               />
             </div>
 
@@ -132,7 +142,7 @@ export default function RateUsForm() {
                 type="text"
                 id="position"
                 name="position"
-                {...register("position")}
+                {...register('position')}
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#FDCD60] focus:ring-2 focus:ring-[#FDCD60] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 placeholder="enter your position "
               />
@@ -145,7 +155,7 @@ export default function RateUsForm() {
               <select
                 id="rateing"
                 name="rateing"
-                {...register("rateing")}
+                {...register('rateing')}
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#FDCD60] focus:ring-2 focus:ring-[#FDCD60] text-base outline-none text-gray-700 py-3 px-3 leading-8 transition-colors duration-200 ease-in-out"
               >
                 <option>1</option>
@@ -165,12 +175,16 @@ export default function RateUsForm() {
               <textarea
                 id="Review"
                 name="message"
-                {...register("message")}
+                {...register('message')}
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#FDCD60] focus:ring-2 focus:ring-[#FDCD60] h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                 placeholder="enter your review"
               ></textarea>
             </div>
-            <div>{status && <div className="text-semibold mb-2 -mt-2">Review Submited</div>}</div>
+            <div>
+              {status && (
+                <div className="text-semibold mb-2 -mt-2">Review Submited</div>
+              )}
+            </div>
             <input
               type="submit"
               className="text-white hover:text-[#FDCD60] bg-[#FDCD60] border-2 border-[#FDCD60] py-2 px-6 focus:outline-none hover:bg-transparent rounded text-lg"
