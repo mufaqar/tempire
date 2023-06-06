0c7147b5f84266078395897b6669b022e16fab75
mufaqar <mufaqar@gmail.com>
2023-06-06 14:26:56 +0500

    applu now page

--- pages/apply.js
+++ pages/apply.js
@@ -19,9 +19,9 @@ export default function Home() {
         <div className='h-full w-full bg-white/70 flex'>
           <div className='container mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2 justify-center items-center z-10 gap-8 md:gap-12'>
             <div className='max-w-[520px] w-full order-2 md:order-none mb-10 md:mb-0'>
-              <h6 className='text-[#edc05f] uppercase font-semibold text-sm'>LEARN ABOUT OUR FINANCING OPTIONS</h6>
+              <h6 className='text-[#edc05f] uppercase font-semibold text-sm bg-gray-800 p-1 inline-block px-2'>LEARN ABOUT OUR FINANCING OPTIONS</h6>
               <h1 className='text-4xl font-bold mt-3 mb-4 font-serif'>Tempire</h1>
-              <p className='font-sans font-medium mb-5'>Our flexibility at TEMPIRE® allows our clients to make the best decisions. We work diligently and closely with each client to source out competitive equipment prices along with partnering with multiple lenders to explore all options so our clients can confidently secure assets for their growing businesses.</p>
+              <p className='font-sans font-medium mb-8'>Our flexibility at TEMPIRE® allows our clients to make the best decisions. We work diligently and closely with each client to source out competitive equipment prices along with partnering with multiple lenders to explore all options so our clients can confidently secure assets for their growing businesses.</p>
               <Link href="https://www.goamur.com/partner-landing/tempire/" className=' px-9 py-3  bg-[#edc05f] hover:text-white hover:bg-gray-900  uppercase font-sans text-sm font-medium shadow-md rounded-full'>APPLY NOW</Link>
             </div>
             <div>
