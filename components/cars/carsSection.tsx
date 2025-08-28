fce783b9c14f4f578e6c8242a3ce2d83a0e80c05
Mufaqar <mufaqar@gmail.com>
2025-08-28 20:23:51 +0500

    cars

--- components/cars/carsSection.tsx
+++ components/cars/carsSection.tsx
@@ -13,19 +13,16 @@ export const CarsSection = () => {
   const indexOfFirstCar = indexOfLastCar - carsPerPage;
   const currentCars = carsData.slice(indexOfFirstCar, indexOfLastCar);
 
-  const totalPages = Math.ceil(carsData.length / carsPerPage);
+  //const totalPages = Math.ceil(carsData.length / carsPerPage);
 
   return (
     <section className="md:pt-[76px] md:pb-[63px] py-[43px] px-4 bg-background">
       <div className="container mx-auto">
-        {/* Car Grid */}
         <div className="grid md:grid-cols-3 grid-cols-1 gap-5 transition-all duration-300 ease-in-out">
           {currentCars.map((car: any, idx: number) => (
             <CarBox key={idx} car={car} />
           ))}
-        </div>
-
-       
+        </div>       
       </div>
     </section>
   );
