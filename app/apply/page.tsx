1f847a24dfa5ff56a0a648c6ea535352a1856daa
unknown <mufaqar@gmail.com>
2025-08-28 06:04:25 -0700

    updated

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: "Apply TEMPIRE | Flexible Equipment Financing Solutions for Growing Businesses",
+  description: "TEMPIRE allows our clients to make the best decisions.",
+};
 
 export default function Apply() {
     return (
--- app/rate-us/page.tsx
+++ app/rate-us/page.tsx
@@ -1,12 +1,18 @@
 import PageBanner from '@/components/pageBanner'
 import RateUsForm from '@/components/rateUs/RateUsForm'
+import { Metadata } from 'next';
 import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: "About TEMPIRE | Flexible Equipment Financing Solutions for Growing Businesses",
+  description: "TEMPIRE allows our clients to make the best decisions.",
+};
 
 export default function Rate_Us() {
   return (
     <>
-    <PageBanner title="Rate Us" />
-
+      <PageBanner title="Rate Us" />
       <RateUsForm />
     </>
   )
